3fe8f868d2463e77abf5ccfbb49975eec0ddc6d1
duasob <zhapebd@gmail.com>
2023-11-21 14:23:39 +0000

    Complete "green" module - still need to test and debug

--- ControlUnit.sv
+++ ControlUnit.sv
@@ -10,4 +10,30 @@ module ControlUnit #(
     logic output                        PCsrc
 );
 
+    logic opcode = instr[6:0]
+    
+    always_comb 
+    case (opcode)
+    7'b0010011: begin //addi instruction
+        RegWrite = 1;
+        ALUctrl = 3'b000;
+        ALUsrc = 1;
+        ImmScr = instr[31:20];
+        PCsrc = 0;
+    end
+    7'b1100011: begin //bne instruction
+        RegWrite = 0;
+        ALUctrl = 3'b000;
+        ALUsrc = 0;
+        ImmScr = {instr[31] , instr[7] , instr[30:25] , instr[11:8]};
+        PCsrc = EQ;
+    end
+        default begin //just in case we have something else
+        RegWrite = 1;
+        ALUctrl = 3'b000;
+        ALUsrc = 1;
+        ImmScr = instr[31:20];
+        PCsrc = 0;
+    end
+    endcase 
 endmodule
