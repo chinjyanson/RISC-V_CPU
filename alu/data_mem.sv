5a22d4ed9d06caf734629cc335f4cb60eed337ea
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-11-30 15:00:07 +0000

    Added writing and reading logic into datamem.sv

--- alu/data_mem.sv
+++ alu/data_mem.sv
@@ -8,5 +8,18 @@ module data_mem #(
     input  logic     [DATA_WIDTH-1:0]        WD,
     output logic     [DATA_WIDTH-1:0]        RD
 );
+    logic [DATA_WIDTH-1:0]  data_mem_register [2**ADDRESS_WIDTH-1:0] //we set our reg file which will be filled with initial values
+
+    initial begin
+        // still missing initial info to write into data mem register
+    end
     
+    always_ff @(posedge clk) begin
+        if (WE == 1'b1) // if write enable is high, we input new data into the [A] register
+            data_mem_register[A] <= WD;
+    end
+
+    assign RD = data_mem_register[A]; //we read and output the [A] register value
+
+
 endmodule
