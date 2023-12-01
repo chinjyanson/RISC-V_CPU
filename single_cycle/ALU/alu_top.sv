2a5ca2aca0f9473b93f1e73622976511607dd509
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-01 12:45:19 +0000

    <style>[Samuel]: <restyled alu>

--- single_cycle/ALU/alu_top.sv
+++ single_cycle/ALU/alu_top.sv
@@ -2,14 +2,14 @@ module alu_top #(
     parameter CONTROL_WIDTH = 3,
     parameter DATA_WIDTH = 32 
 )(
-    input   wire                    clk,
-    input   wire                    ALUsrc,
-    input   wire [2:0]              ALUctrl,
-    input   wire [DATA_WIDTH-1:0]   Instr,
-    input   wire                    RegWrite,
-    input   wire [DATA_WIDTH-1:0]   ImmOp,
-    output  wire                    Zero,
-    output  wire [DATA_WIDTH-1:0]   a0
+    input   wire                        clk,
+    input   wire                        ALUsrc,
+    input   wire [CONTROL_WIDTH-1:0]    ALUctrl,
+    input   wire [DATA_WIDTH-1:0]       Instr,
+    input   wire                        RegWrite,
+    input   wire [DATA_WIDTH-1:0]       ImmOp,
+    output  wire                        Zero,
+    output  wire [DATA_WIDTH-1:0]       a0
 );
 
 wire [DATA_WIDTH-1:0] ALUResult;
@@ -18,28 +18,28 @@ wire [DATA_WIDTH-1:0] SrcB;
 wire [DATA_WIDTH-1:0] regOp2;
 
 regfile register(
-    .clk(clk),
-    .Instr(Instr),
-    .WE3(RegWrite),
-    .WD3(ALUResult),
-    .RD1(SrcA),
-    .RD2(regOp2),
-    .a0(a0)
+    .clk        (clk),
+    .Instr      (Instr),
+    .WE3        (RegWrite),
+    .WD3        (ALUResult),
+    .RD1        (SrcA),
+    .RD2        (regOp2),
+    .a0         (a0)
 );
 
-mux2 ALUMux(
-    .ALUsrc(ALUsrc),
-    .regOp2(regOp2),
-    .ImmOp(ImmOp),
-    .ALUop2(SrcB)
+mux2 ALUMux( // checked - SK 1/12/2023
+    .control    (ALUsrc),
+    .input0     (regOp2),
+    .input1     (ImmOp),
+    .out        (SrcB)
 );
 
-alu ALU(
-    .ALUctrl(ALUctrl),
-    .ALUop1(SrcA),
-    .ALUop2(SrcB),
-    .SUM(ALUResult),
-    .Zero(Zero)
+alu ALU( // checked - SK 1/12/2023
+    .ALUctrl    (ALUctrl),
+    .SrcA       (SrcA),
+    .SrcB       (SrcB),
+    .ALUResult  (ALUResult),
+    .Zero       (Zero)
 );
 
 endmodule
