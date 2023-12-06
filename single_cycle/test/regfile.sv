8ffeba6d598be992f948ba5090ea9334d4913b1a
duasob <zhapebd@gmail.com>
2023-12-06 17:40:02 +0000

    test2

--- single_cycle/test/regfile.sv
+++ single_cycle/test/regfile.sv
@@ -3,7 +3,7 @@ module regfile #(
     parameter DATA_WIDTH = 32
 )(
     input   logic                   clk,
-    input   logic                   WE3,
+    input   logic [1:0]             WE3,
     input   logic [DATA_WIDTH-1:0]  WD3,
     input   logic [DATA_WIDTH-1:0]  Instr,
     output  logic [DATA_WIDTH-1:0]  RD1,
@@ -11,27 +11,31 @@ module regfile #(
     output  logic [DATA_WIDTH-1:0]  a0  //(debug output)
 );
 
-logic[4:0] A2 = Instr[24:20];
-logic[4:0] A1 = Instr[19:15];
-logic[4:0] A3 = Instr[11:7];
+logic[7:0] A2 = {3'b0 , Instr[24:20]};
+logic[7:0] A1 = {3'b0 , Instr[19:15]};
+logic[7:0] A3 = {3'b0 , Instr[11:7]};
 
 logic [DATA_WIDTH-1:0] reg_array [2**ADDRESS_WIDTH-1:0];
 
-
 always_ff @(posedge clk) begin 
-    if((WE3 == 1'b1) && (A3 != 0)) begin
-        reg_array[A3] <= WD3;
-    end 
+    case(WE3) // this could be done cleaner 
+        2'b01: begin //word write
+            reg_array[A3] <= WD3;
+        end
+        2'b10: begin //half word 
+            reg_array[A3][15:0] <= WD3[15:0];
+        end
+        2'b11:begin //write byte
+            reg_array[A3][7:0] <= WD3[7:0];
+        end
+        default: 
+            //dont do anything?
+    endcase
 end 
-//assign reg_array[0] = 0; //zero
 
 assign RD1 = reg_array[A1];
 assign RD2 = reg_array[A2];
-assign a0 = reg_array[10];
-
-
-
-
-
+assign a0 =  reg_array[10];
 
 endmodule
+
