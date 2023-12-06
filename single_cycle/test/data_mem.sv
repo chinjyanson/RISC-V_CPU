5a435b5dee0bca728deb4ccc6681a21f37b07e95
duasob <zhapebd@gmail.com>
2023-12-06 16:23:28 +0000

    <feat> [duasob]: implement byte/half WE data_mem

--- single_cycle/test/data_mem.sv
+++ single_cycle/test/data_mem.sv
@@ -1,7 +1,6 @@
 module data_mem #(
     parameter ADDRESS_WIDTH = 8,
-    parameter DATA_WIDTH = 32,
-    parameter REG_WIDTH = 8 //convert to byte addressing
+    parameter DATA_WIDTH = 32
 ) (
     input  logic                             clk,
     input  logic                             WE,
@@ -9,7 +8,7 @@ module data_mem #(
     input  logic     [DATA_WIDTH-1:0]        WD,
     output logic     [DATA_WIDTH-1:0]        RD
 );
-    logic [REG_WIDTH-1:0]  data_mem_register [2**ADDRESS_WIDTH-1:0]; //we set our reg file which will be filled with initial values
+    logic [DATA_WIDTH-1:0]  data_mem_register [2**ADDRESS_WIDTH-1:0] //we set our reg file which will be filled with initial values
 
     initial begin
         $display("Loading ROM");
@@ -18,12 +17,9 @@ module data_mem #(
     
     always_ff @(posedge clk) begin
         if (WE == 1'b1) // if write enable is high, we input new data into the [A] register
-            data_mem_register[A] <= WD[7:0];
-            data_mem_register[A+1] <= WD[15:8];
-            data_mem_register[A+2] <= WD[23:16];
-            data_mem_register[A+3] <= WD[31:24];
+            data_mem_register[A] <= WD;
     end
 
-    assign RD = {data_mem_register[A], data_mem_register[A+1], data_mem_register[A+2], data_mem_register[A+3]}; //we read and output the [A] register value
+    assign RD = data_mem_register[A]; //we read and output the [A] register value
 
 endmodule
