55c61ec9eda60ae692f19bba4ed4ceee232fc445
duasob <zhapebd@gmail.com>
2023-12-06 17:09:11 +0000

    <fix> [duasob]:revert to word addressing data_mem

--- single_cycle/ALU/data_mem.sv
+++ single_cycle/ALU/data_mem.sv
@@ -1,7 +1,6 @@
 module data_mem #(
     parameter ADDRESS_WIDTH = 8,
-    parameter DATA_WIDTH = 32,
-    parameter REG_WIDTH = 8 //convert to byte addressing
+    parameter DATA_WIDTH = 32
 ) (
     input  logic                             clk,
     input  logic     [1:0]                   WE,
@@ -17,7 +16,7 @@ module data_mem #(
         10 - write half word (16 bits)
         11 - write last byte (8 bits)
     */   
-    logic [REG_WIDTH-1:0]  data_mem_register [2**ADDRESS_WIDTH-1:0]; //we set our reg file which will be filled with initial values
+    logic [DATA_WIDTH-1:0]  data_mem_register [2**ADDRESS_WIDTH-1:0]; //we set our reg file which will be filled with initial values
 
     initial begin
         $display("Loading ROM");
@@ -27,22 +26,18 @@ module data_mem #(
     always_ff @(posedge clk) 
         case(WE) // this could be done cleaner 
         2'b01: begin //word write
-            data_mem_register[A] <= WD[7:0];
-            data_mem_register[A+1] <= WD[15:8];
-            data_mem_register[A+2] <= WD[23:16];
-            data_mem_register[A+3] <= WD[31:24];
+            data_mem_register[A] <= WD;
         end
         2'b10: begin //half word 
-            data_mem_register[A] <= WD[7:0];
-            data_mem_register[A+1] <= WD[15:8];
+            data_mem_register[A][15:0] <= WD[15:0];
         end
         2'b11:begin //write byte
-            data_mem_register[A] <= WD[7:0];
+            data_mem_register[A][7:0] <= WD[7:0];
         end
         default: 
             //dont do anything?
         endcase
 
-    assign RD = {data_mem_register[A], data_mem_register[A+1], data_mem_register[A+2], data_mem_register[A+3]}; //we read and output the [A] register value
+    assign RD = data_mem_register[A]; //we read and output the [A] register value
 
 endmodule
