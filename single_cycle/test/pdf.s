a00343487c6cf87d0d8e36b34d373588a307f7be
chinjyanson <anson.chin22@imperial.ac.uk>
2023-12-07 14:21:47 +0000

    <feat> [ayc122]: added the new f1 assembly code

--- single_cycle/test/pdf.s
+++ single_cycle/test/pdf.s
@@ -1,20 +1,21 @@
 .text
 .equ base_pdf, 0x100
 .equ base_data, 0x10000
-.equ max_count, 200
+.equ max_count, 500
 main:
     JAL     ra, init  # jump to init, ra and save position to ra
     JAL     ra, build
 forever:
     JAL     ra, display
-    J       forever
+    JAL     forever
 
 init:       # function to initialise PDF buffer memory 
-    LI      a1, 0x100           # loop_count a1 = 256
+    LI      a1, 0x100
 _loop1:                         # repeat
-    ADDI    a1, a1, -1          #     decrement a1
-    SB      zero, base_pdf(a1)  #     mem[base_pdf+a1) = 0
-    BNE     a1, zero, _loop1    # until a1 = 0
+    ADDI a1, zero, 255
+    ADDI a0, zero, 0
+    SB zero, base_pdf(a1)
+    BEQ a0, a1, _loop1
     RET
 
 build:      # function to build prob dist func (pdf)
@@ -23,14 +24,10 @@ build:      # function to build prob dist func (pdf)
     LI      a3, base_pdf        # a3 = base address of pdf array
     LI      a4, max_count       # a4 = maximum count to terminate
 _loop2:                         # repeat
-    ADD     a5, a1, a2          #     a5 = data base address + offset
-    LBU     t0, 0(a5)           #     t0 = data value
-    ADD     a6, t0, a3          #     a6 = index into pdf array
-    LBU     t1, 0(a6)           #     t1 = current bin count
-    ADDI    t1, t1, 1           #     increment bin count
-    SB      t1, 0(a6)           #     update bin count
-    ADDI    a2, a2, 1           #     point to next data in array
-    BNE     t1, a4, _loop2      # until bin count reaches max
+    SLLI    a0, a0, 1 
+    ADDI    a0, a0, 1
+    LI      t0, 0xFF
+    BNE     a0, t0, _loop2      # until bin count reaches max
     RET
 
 display:    # function send PDF array value to a0 for display
