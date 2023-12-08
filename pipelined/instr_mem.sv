e31e78940a98fd20bc7fe8a1b3df8d07b401541e
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-08 21:14:18 +0000

    <fix> [ssk122]: added new instrmem

--- pipelined/instr_mem.sv
+++ pipelined/instr_mem.sv
@@ -20,3 +20,23 @@ end;
 assign Instr_o = {{rom_array[addr_i+3]}, {rom_array[addr_i+2]}, {rom_array[addr_i+1]}, {rom_array[addr_i]}};
 
 endmodule
+
+module instr_mem #(
+    parameter   ADDRESS_WIDTH = 8,
+    parameter   DATA_WIDTH = 32
+)(
+    input logic [ADDRESS_WIDTH-1:0] pc_i,
+    output logic [DATA_WIDTH-1:0]   instr_o
+);
+
+    logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+    
+    initial begin
+        $readmemh("pdf.mem", rom_array);
+    end;
+
+    always_comb begin
+        instr_o = rom_array[pc_i];
+    end
+
+endmodule
