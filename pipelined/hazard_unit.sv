287a24ebdac143981b31b5eda0972c7d0da1801f
duasob <zhapebd@gmail.com>
2023-12-09 15:34:59 +0000

    <feat> [duasob]: <fowarding for hazard_unit>

--- pipelined/hazard_unit.sv
+++ pipelined/hazard_unit.sv
@@ -4,19 +4,28 @@ module hazard_unit #(
     parameter   CONTROL_WIDTH = 3,
     parameter   IMM_WIDTH = 2
 )(
-    logic input [4:0]   Rs1E,
-    logic input [4:0]   Rs2E,
-    logic input [4:0]   RdM, 
-    logic input [4:0]   RdW,
-    logic input [2:0]   RegWriteW,
-    logic output[1:0]   FowardAE,
-    logic output[1:0]   FowardBE,
+    /*
+    logic input [6:0]   opcode,
+    logic input [2:0]   funct3,
+    logic input         funct7,  
+    */
+    logic input [4:0]   Rs1E_i,
+    logic input [4:0]   Rs2E_i,
+    logic input [4:0]   RdM_i, 
+    logic input [4:0]   RdW_i,
+    logic input [2:0]   RegWriteW_i,
+    logic output[1:0]   FowardAE_o,
+    logic output[1:0]   FowardBE_o
     
     //logic shown in the slides, we will need more 
 );
 
 data_hazard DataHazard(
-
+    .RdM(RdM_i),
+    .Rs1E(Rs1E_i),
+    .Rs2E(Rs2E_i),
+    .FowardAE(FowardAE_o),
+    .FowardBE(FowardBE_o)
 );
 
 control_hazard ControlHazard(
