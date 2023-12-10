0aad7ea867e67a8cb0fe55ba6009cfd966f5bf22
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-10 15:01:42 +0000

    <style> [ssk122]: restyled adder.sv to fit style

--- pipelined/adder.sv
+++ pipelined/adder.sv
@@ -1,10 +1,11 @@
 module adder #(
     parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1:0]    input0,
-    input logic [DATA_WIDTH-1:0]    input1,
-    output logic [DATA_WIDTH-1:0]   out
+    input   logic [DATA_WIDTH-1:0]    input0,
+    input   logic [DATA_WIDTH-1:0]    input1,
+    output  logic [DATA_WIDTH-1:0]    out
 );
 
 assign out = input0 + input1;
+
 endmodule
