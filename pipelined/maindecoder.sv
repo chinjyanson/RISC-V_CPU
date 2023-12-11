bce80a1e9231abb6242c292f476eadbaeffa016b
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-11 00:53:10 +0000

    <fix> [ssk122]: Organised decode.sv files

--- pipelined/maindecoder.sv
+++ pipelined/maindecoder.sv
@@ -1,8 +1,12 @@
 module maindecoder #(
-    parameters  OP_WIDTH = 7,
-    parameters  IMM_WIDTH = 2
+    parameters  OP_WIDTH = 7
 ) (
-    port_list
+    input   logic [OP_WIDTH-1:0]    opcode_i,
+    output  logic [1:0]             ResultSrc,
+    output  logic                   MEMWrite_o,
+    output  logic                   Branch,
+    output  logic []                ALUSrc,
+
 );
     
 endmodule
