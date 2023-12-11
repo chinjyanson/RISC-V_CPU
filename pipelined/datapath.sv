414bb4b04909a5d2ac495218cfd38dd08f706762
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-11 21:04:48 +0000

    <feat> [ssk122]: <added more ports to datapath.sv>

--- pipelined/datapath.sv
+++ pipelined/datapath.sv
@@ -5,7 +5,23 @@
 module datapath #(
     parameter DATA_WIDTH = 32
 ) (
-    port_list
+    input   logic                   clk,
+    input   logic                   reset,
+
+    input   logic [1:0]             ResultSrcW,
+    input   logic                   PCJalSrcE,
+    input   logic                   PCSrcE,
+    input   logic                   ALUSrcE,
+    input   logic                   RegWriteW,
+    input   logic [2:0]             ImmSrcD,
+    input   logic [2:0]             ALUControlD,
+
+    output  logic                   ZeroE,
+    output  logic                   PCF,
+    input   logic [DATA_WIDTH-1:0]  InstrF,
+    input   logic [DATA_WIDTH-1:0]   
+
 );
-    
+
+logic [DATA_WIDTH-1:0] PCD, PCE, ALUResultE, ALUResultW, ReadDataW;
 endmodule
