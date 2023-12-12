fafbd1101bb423987224f288050e2d2b3705a6ce
duasob <zhapebd@gmail.com>
2023-12-12 13:19:27 +0000

    <fix> [duasob]: <change alu/aludecoder for lui>

--- pipelined/aludecoder.sv
+++ pipelined/aludecoder.sv
@@ -13,8 +13,8 @@ assign RtypeSub = funct7b5 & opb5; //TRUE for R-type subtract
 
     always_comb
         case (ALUOp)
-            2'b00 = ALUControl = 3'b000; // addition
-            2'b01 = ALUControl = 3'b001; // subtraction
+            2'b00: ALUControl = 3'b000; // addition
+            2'b01: ALUControl = 3'b001; // subtraction
             default: // made it a default but in theory just ALUOp = 2'b10
                 case (funct3)
                     3'b000: ALUControl = (RtypeSub) ? 3'b001 : 3'b000; // takes into account case where RtypeSub gives add or addi or sub
@@ -22,6 +22,7 @@ assign RtypeSub = funct7b5 & opb5; //TRUE for R-type subtract
                     3'b010: ALUControl = 3'b101; // set less than
                     3'b110: ALUControl = 3'b011; // or
                     3'b111: ALUControl = 3'b010; // and 
+                                                    //need to add something for lui
                     default: ALUControl = 3'bxxx;
                 endcase
         endcase
