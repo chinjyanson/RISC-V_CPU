acde1c75e5691501130162603724b11fb217c07e
SamuelKhoo2003 <samuelkhoo20032004@gmail.com>
2023-12-14 12:11:25 +0000

    <fix> [ssk122]: <removed redundant code>

--- pipelined/maindecoder.sv
+++ pipelined/maindecoder.sv
@@ -21,22 +21,22 @@ always_comb
     case (op)
         // RegWrite_ResultSrc_MemWrite_Jump_Branch_ALUSrc_ImmSrc_ALUOp
         7'b0000011:begin
-            controls = 15'b000_01_00_0_0_1_000_00; // lw
+            controls = 15'b001_01_00_0_0_1_000_00; // lw
             case(funct3)
                 3'b000: controls = {{3'b011}, controls [11:0]}; //lb
                 3'b001: controls = {{3'b010}, controls [11:0]}; //lh
-                3'b010: controls = {{3'b001}, controls [11:0]}; //lw
+                // 3'b010: controls = {{3'b001}, controls [11:0]}; //lw
                 3'b100: controls = {{3'b111}, controls [11:0]}; //lbu
                 3'b101: controls = {{3'b110}, controls [11:0]}; //lhu
                 default: controls = controls; 
             endcase
         end
         7'b0100011: begin
-            controls = 15'b000_00_00_0_0_1_010_00; // sw
+            controls = 15'b000_00_01_0_0_1_010_00; // sw
             case(funct3)
                 3'b000: controls = {controls[14:10],{2'b11}, controls[7:0]}; //sb
                 3'b001: controls = {controls[14:10],{2'b10}, controls[7:0]}; //sh
-                3'b010: controls = {controls[14:10],{2'b01}, controls[7:0]}; //sw
+                // 3'b010: controls = {controls[14:10],{2'b01}, controls[7:0]}; //sw
                 default : controls = controls;
             endcase
         end
