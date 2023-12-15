9ea9569e56986f1d0bb6608c683082e3ace65986
vishesh32 <wish.mongia@gmail.com>
2023-12-15 01:22:59 +0000

    <debug>

--- pipelined/regfile.sv
+++ pipelined/regfile.sv
@@ -23,16 +23,25 @@ logic [4:0] A3 = A3_i;
 
 logic [DATA_WIDTH-1:0] reg_array[2**ADDRESS_WIDTH-1:0];
 
-always @(negedge clk) begin
-    if ((A3 != 0)&&(WD3)) begin
+always_ff @(posedge clk) begin
+    if ((WD3)) begin
+        if (A3==5'b00000)
+            reg_array[A3] <= 32'b0;
+    else
         reg_array[A3] <= WD3; // write
     end
 end
 
+always_ff @(negedge clk) begin 
+    RD1<=reg_array[A1];
+    RD2<=reg_array[A2];
+    a0<= reg_array[10];
+    if (A1 ==5'b00000)
+        RD1<=5'b0;
+    if(A2 == 5'b00000)
+        RD2<= 5'b0;
+end
 
-assign RD1 = reg_array[A1];
-assign RD2 = reg_array[A2];
-assign a0 =  reg_array[6];
 
 endmodule
 
